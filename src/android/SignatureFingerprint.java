355b20faa6a2a673d9c7e2b4096511e393fda3aa
Samuel Fontebasso <fontebasso@mbp-de-samuel.home>
2017-08-01 20:51:23 -0300

    Fix bugs

--- src/android/SignatureFingerprint.java
+++ src/android/SignatureFingerprint.java
@@ -6,6 +6,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.pm.PackageManager;
+
 public class SignatureFingerprint extends CordovaPlugin {
 
     @Override
@@ -14,6 +16,11 @@ public class SignatureFingerprint extends CordovaPlugin {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
+        }
+        if (action.equals("getVersionCode")) {
+          PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+          callbackContext.success(packageManager.getPackageInfo(this.cordova.getActivity().getPackageName(), 0).versionCode);
+          return true;
         }
         return false;
     }
