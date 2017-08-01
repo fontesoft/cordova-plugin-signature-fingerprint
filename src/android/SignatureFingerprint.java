7afda26c8359f9489b0de872d101b244e7f99c1a
Samuel Fontebasso <fontebasso@mbp-de-samuel.home>
2017-08-01 20:27:28 -0300

    Fix bugs

--- src/android/SignatureFingerprint.java
+++ src/android/SignatureFingerprint.java
@@ -6,23 +6,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.pm.PackageManager.NameNotFoundException;
-
 public class SignatureFingerprint extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-      try {
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
         }
         return false;
-      } catch (NameNotFoundException e) {
-        callbackContext.success("N/A");
-        return true;
-      }
     }
 
     private void coolMethod(String message, CallbackContext callbackContext) {
